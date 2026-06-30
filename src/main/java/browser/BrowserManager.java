154f2e61d21f796c9cba2c8e15156dbcc1d9571d
Bhabani Shankar Rout <bsrout0@gmail.com>
2026-06-30 16:07:39 +0530

    commit

---
+++ src/main/java/browser/BrowserManager.java
@@ -0,0 +1,34 @@
+package browser;
+
+import com.microsoft.playwright.*;
+
+import java.awt.*;
+
+public class BrowserManager {
+    public Playwright playwright; //used to create an instance of the Chromium, Firefox browser etc.
+    public Page page; //is the single tab or window in the browser.
+    public BrowserContext context; //is the isolated browser session.
+    public Browser browser; //represents the browser instance.
+
+    public void setUp() {
+        System.out.println("Setting up Playwright...");
+        // Get viewport size of screen
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = (int) screenSize.getWidth();
+        int height = (int) screenSize.getHeight();
+
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        context = browser.newContext(new Browser.NewContextOptions().setViewportSize(width, height));
+        page = context.newPage();
+        System.out.println("Playwright setup complete!");
+    }
+
+    public void tearDown() {
+        System.out.println("Tearing down Playwright...");
+        if (page != null) page.close();
+        if (browser != null) browser.close();
+        if (playwright != null) playwright.close();
+        System.out.println("Playwright teardown complete!");
+    }
+}
